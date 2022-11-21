a0d0fa930855e63819a713e96d7a23d1e73a2de6
Wanjiku Kimani <wanjikukimani24@gmail.com>
2022-11-21 13:03:07 +0300

    Completed test

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+const currentUser = 'Faith Kimani';
+let welcomeMessage = "Welcome to Flatbook, "
+
+//Using string concatenation
+ welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+
+// using template literals
+welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+//Changing to uppercase
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//slicing
+const firstInitial = currentUser.slice(0,1);
+// const restOfName = currentUser.slice
+const shortGreeting = `Welcome, ${firstInitial}!`;
+
